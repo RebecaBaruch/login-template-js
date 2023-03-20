3c2772977b2c8e443a6f05a953dc5a0df302c451
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2023-03-20 10:33:43 -0300

    feat: validation

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,23 @@ const inputs = document.querySelectorAll("input");
 
 //regex validation
 let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-let passwRegex = /^[A-Za-z]\w{7,14}$/;
+let passwRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
 
-const inputVal = (e, regex)=>{
-    e.value.match(regex)
+let emailErr = "Por favor chefe, insira um email válido!!!!";
+let passwErr = "A senha deve ter 8 caracteres (maiúsculos, minpusculos e números)";
+
+//focus style event
+const focusIn = (input)=>{
+    input.addEventListener("focus", ()=>{
+        input.classList.add("focusIn");
+    })
 }
 
-//focus out validation
+//focus out style event
 const focusOut = (input)=>{
     input.addEventListener("focusout", ()=>{
+        input.classList.remove("focusIn");
+
         if(input.value == "" || input.value == null){
             input.classList.add("focusOut");
         } else {
@@ -19,11 +27,28 @@ const focusOut = (input)=>{
     })
 }
 
-inputs.forEach((e) => {
-    // e.addEventListener("input", ()=>{
-    //     console.log(e.value)
-    // })
+//input validation
+const inputValidation = (input, regex, message) =>{
+    
+    input.addEventListener("input", ()=>{
+        let test = regex.test(input.value);
 
+        test == false ? 
+        input.classList.add("inputErr"):
+        input.classList.remove("inputErr");
+    })  
+
+    input.addEventListener("blur", ()=>{
+        let test = regex.test(input.value);
+        
+        if(test == false) alert(message);
+    })
+}
+
+inputs.forEach((e) => {
     focusOut(e);
-   
+    focusIn(e);
+
+    if(e.type == "email") inputValidation(e, emailRegex, emailErr);
+    if(e.type == "password") inputValidation(e, passwRegex, passwErr);
 });
